c647ce3cad917e58812ec3c69e8479e172a4a174
SergejBoyko <sergej13boyko@mail.ru>
2023-04-12 04:49:36 +0700

    Добавьте файлы проекта.

---
+++ 8.6.4/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace FinalTask // Объявление пространства имен для данного кода
+{
+    [Serializable]class Student
+    {
+        public string Name { get; set; }
+        public string Group { get; set; }
+        public DateTime Age { get; set; }
+        public Student(string name, string group, DateTime age)
+        {
+            Name = name;
+            Group = group;
+            Age = age;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string DataBase = "C:\\Users\\B1S3\\Desktop\\testFolder12\\Students.dat";
+            Directory.CreateDirectory("C:\\Users\\B1S3\\Desktop\\Student");
+            BinaryFormatter formatter = new BinaryFormatter();
+            using (var fs = new FileStream(DataBase, FileMode.OpenOrCreate))
+            {
+                var newStudent = (Student)formatter.Deserialize(fs);
+                Console.WriteLine("Объект десериализован");
+                Console.WriteLine($"Имя: {newStudent.Name} Группа: {newStudent.Group} --- Возраст: {newStudent.Age}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
+            
